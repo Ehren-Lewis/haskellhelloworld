d14a8427383f5e9f42f2225b103ca119000873f5
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2022-12-04 01:31:09 -0600

    Implemented a last but 1 function in haskell using recursion

--- app/hello-world/app/Main.hs
+++ app/hello-world/app/Main.hs
@@ -3,27 +3,29 @@ data MyType a
     = MyBool Bool | MyAnything a
     | MyString String
 
--- data BoolTest 
-    -- = 
-
-data Peson =
-    Student String Int Int | Teacher String Int 
-
-freddy :: Person
-
-var1 :: MyType Int -> String
--- var1 :: MyType -> BoolTest -> String
-var1 (MyBool a) = show a 
-var1 (MyString a)  = show a
-var1 ( MyAnything a) = show a
-
-data MyTuple a b = MyTuple a b deriving Show 
 
 
-myswap :: MyTuple a b -> MyTuple b a 
-myswap (MyTuple a b) = MyTuple b a
+
+-- var1 :: MyType Int -> String
+-- -- var1 :: MyType -> BoolTest -> String
+-- var1 (MyBool a) = show a 
+-- var1 (MyString a)  = show a
+-- var1 ( MyAnything a) = show a
+
+-- data MyTuple a b = MyTuple a b deriving Show 
+
+
+-- myswap :: MyTuple a b -> MyTuple b a 
+-- myswap (MyTuple a b) = MyTuple b a
+
+lastButOne :: [a] -> [a] 
+lastButOne list  = if length list == 2
+                    then take 1 list
+                    else lastButOne (tail list)
+
 
 
 main :: IO ()
-main = putStrLn (var1 (MyBool True))
+main = print ( lastButOne [1, 2, 3, 4, 5])
+
 
