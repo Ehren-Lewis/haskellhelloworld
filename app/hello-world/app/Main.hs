f46925098bec23fb51f2ceedabb9f377d905ca86
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2022-12-07 00:19:48 -0600

    Worked on creating a parenthesis parser

--- app/hello-world/app/Main.hs
+++ app/hello-world/app/Main.hs
@@ -1,4 +1,5 @@
 module Main where
+import Data.Char  
 -- data MyType a
 --     = MyBool Bool | MyAnything a
 --     | MyString String
@@ -17,18 +18,79 @@ module Main where
 
 -- type synonym
 -- used to make code more readable
-type Age = Int
-type Title = String
+-- type Age = Int
+-- type Title = String
 
-data Person = Person Int String deriving (Show)
+-- data Person = Person Int String deriving (Show)
 
 -- fred = Person 21 "freddy"
 
 -- personName ( Person age name) = name
-personAge :: Perso -> String
-personAge  (Person age title ) = age
-personName  (Person age title ) = title
+-- personAge :: Person -> String
+-- personAge  (Person age title ) = age
+-- personName  (Person age title ) = title
 
+-- data Person2 = Person2 {
+--     personName2 :: PersonName2,
+--     personAge2 :: PersonAge2,
+-- } deriving ( Show )
+
+data Optional a 
+    = None 
+    | Some a deriving Show
+
+parseOpenParen :: String -> (String, Optional Char)
+parseOpenParen "" = ("", None)
+parseOpenParen (c : cs) = 
+  if c == '('
+    then (cs, Some '(') 
+    else (c : cs, None)
+
+
+parseCloseParen :: String -> (String, Optional Char)
+parseCloseParen "" = ("", None)
+parseCloseParen (c : cs) = 
+  if c == ')'
+    then (cs, Some ')') 
+    else (c : cs, None)
+
+
+parseAlpha :: String -> ([Char], Optional Char)
+parseAlpha "" = ("", None)
+parseAlpha (c: cs) =
+  if isAlpha c
+    then (cs, Some c)
+    else (c: cs, None)
+
+
+-- newParseAlpha :: String -> ( String, Optional Char)
+
+
+parse :: String -> (String, Optional String)
+parse str =
+  case parseOpenParen str of 
+    ( _, None) -> ( str, None)
+    ( str', Some c1) -> 
+      case parseAlpha str' of 
+        ( _, None) -> ( str', None)
+        ( str'', Some c2) -> 
+          case parseCloseParen str'' of 
+            ( _, None) -> ( str'', None)
+            ( str''', Some c3) -> ( str''', Some [c1,c2, c3 ])
+
+-- parse2 :: String -> (String, Optional String)
+-- parse2 str =
+--   case parseOpenParen str of 
+--     ( _, None) -> ( str, None)
+--     ( str', Some c1) -> 
+--       case parseAlpha str' of 
+--         ( _, None) -> ( str', None)
+--         ( str'', Some c2) -> 
+--           case parseCloseParen str'' of 
+--             ( _, None) -> ( str'', None)
+--             ( str''', Some c3) -> ( str''', Some [c1,c2, c3 ])
+
+  
 
 -- var1 :: MyType Int -> String
 -- -- var1 :: MyType -> BoolTest -> String
